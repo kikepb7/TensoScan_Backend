49800b2200f3690a425c2b221304953de0135701
kikepb7 <enriquepalmadev@gmail.com>
2025-04-20 04:58:43 +0200

    PROJECT | Implement database config

--- app/core/config.py
+++ app/core/config.py
@@ -11,8 +11,11 @@ class Settings(BaseSettings):
     MODEL_PATH: str = os.getenv("MODEL_PATH", "ia_models/")
     FIREBASE_CREDENTIALS: str = os.getenv("FIREBASE_CREDENTIALS")
     OPENAI_API_KEY: str
-    MONGO_URI = "mongodb://localhost:27017"
-    MONGO_DB = "chatbot_db"
+    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+    DB_NAME = os.getenv("DB_NAME", "ocr_db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRES_MINUTES = 30
 
     class Config:
         env_file = ".env"
