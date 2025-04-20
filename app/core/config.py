78a6248a6ae04e966665b0c83599d7973f58acda
kikepb7 <enriquepalmadev@gmail.com>
2025-04-20 22:20:43 +0200

    PROJECT | Update requirements.txt and config.py files

--- app/core/config.py
+++ app/core/config.py
@@ -6,16 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
+    # App Settings
     API_PORT: int = int(os.getenv("API_PORT", 8000))
     DEBUG: bool = os.getenv("DEBUG", "True").lower() == "true"
+
+    # AI Model
     MODEL_PATH: str = os.getenv("MODEL_PATH", "ia_models/")
-    FIREBASE_CREDENTIALS: str = os.getenv("FIREBASE_CREDENTIALS")
-    OPENAI_API_KEY: str
-    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-    DB_NAME = os.getenv("DB_NAME", "ocr_db")
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRES_MINUTES = 30
+
+    # API Keys
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+
+    # MongoDB
+    MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+    DB_NAME: str = os.getenv("DB_NAME", "tenso_scan_db")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
